fefb61b4b1e05c644548ef79515c5d82e4500f2e
hydrationn <suhwaparkk1031@gmail.com>
2024-03-09 20:48:57 +0900

    AccountServiceImpl 구현

--- src/main/java/com/nhnacademy/springbootminidooray3accountapi/controller/AccountController.java
+++ src/main/java/com/nhnacademy/springbootminidooray3accountapi/controller/AccountController.java
@@ -15,33 +15,38 @@ public class AccountController {
         this.accountService = accountService;
     }
 
-    @PostMapping("/login") // 로그인 api
+    // 로그인 api
+    @PostMapping("/login")
     public String login(@RequestParam String id, @RequestParam String password) {
         return accountService.login(id, password);
     }
 
-    @PostMapping("/signup") // 회원가입 api
+    // 회원가입 api
+    @PostMapping("/signup")
     public Account createAccount(@RequestBody Account account) {
         return accountService.createAccount(account);
     }
 
-    @GetMapping("/accounts/{id}") // 회원 정보 조회 api
+    // 회원 정보 조회 api
+    @GetMapping("/accounts/{id}")
     public Optional<Account> getAccount(@PathVariable String id) {
         return accountService.getAccount(id);
     }
 
-    @GetMapping("/accounts") // 회원 리스트 조회 api
+    // 회원 리스트 조회 api
+    @GetMapping("/accounts")
     public List<Account> getAccounts() {
         return accountService.getAccounts();
     }
 
-    @PutMapping("/accounts/{id}/state") // 회원 상태 수정 api
+    // 회원 상태 수정 api
+    @PutMapping("/accounts/{id}/state")
     public String updateAccountState(@PathVariable String id, @RequestParam String state) {
         return accountService.updateAccountState(id, state);
     }
 
-
-    @DeleteMapping("/accounts/{id}") // 회원 정보 삭제 api
+    // 회원 정보 삭제 api
+    @DeleteMapping("/accounts/{id}")
     public void deleteAccount(@PathVariable String id) {
         accountService.deleteAccount(id);
     }
--- src/main/java/com/nhnacademy/springbootminidooray3accountapi/service/AccountServiceImpl.java
+++ src/main/java/com/nhnacademy/springbootminidooray3accountapi/service/AccountServiceImpl.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.springbootminidooray3accountapi.service;
 
 import com.nhnacademy.springbootminidooray3accountapi.entity.Account;
+import com.nhnacademy.springbootminidooray3accountapi.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,33 +10,64 @@ import java.util.Optional;
 
 @Service
 public class AccountServiceImpl implements AccountService {
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    // 로그인
     @Override
     public String login(String id, String password) {
-        return null;
+        Optional<Account> optionalAccount = accountRepository.findById(id);
+        if (optionalAccount.isPresent()) {
+            Account account = optionalAccount.get();
+            if (account.getPassword().equals(password)) {
+                return "로그인 성공";
+            } else {
+                return "비밀번호 입력 오류";
+            }
+        } else {
+            return "존재하지 않는 계정";
+        }
     }
 
+    // 회원가입
     @Override
     public Account createAccount(Account account) {
-        return null;
+        return accountRepository.save(account);
     }
 
+    // 회원 정보 조회
     @Override
     public Optional<Account> getAccount(String id) {
-        return Optional.empty();
+        return accountRepository.findById(id);
     }
 
+    // 회원 리스트 조회
     @Override
     public List<Account> getAccounts() {
-        return null;
+        return accountRepository.findAll();
     }
 
+    // 회원 상태 수정
     @Override
     public String updateAccountState(String id, String state) {
-        return null;
+        Optional<Account> optionalAccount = accountRepository.findById(id);
+        if (optionalAccount.isPresent()) {
+            Account account = optionalAccount.get();
+            account.setState(state);
+            accountRepository.save(account);
+            return "회원 상태 업데이트";
+        } else {
+            return "존재하지 않는 계정";
+        }
     }
 
+    // 회원 정보 삭제
     @Override
     public void deleteAccount(String id) {
-
+        accountRepository.deleteById(id);
     }
 }
